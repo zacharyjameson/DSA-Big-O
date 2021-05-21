bb7206a2bc26acefd72f76a3f2fb050d6f1dcc00
Zack Jameson <zacharymjameson@gmail.com>
2021-05-21 15:47:27 -0400

    even or odd

---
+++ even-or-odd.js
@@ -0,0 +1,12 @@
+//2. Even or odd
+
+//What is the Big O of the following algorithm? Explain your answer
+
+function isEven(value) {
+    if (value % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
