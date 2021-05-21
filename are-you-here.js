e1c00363abce1105793b60d36a9c4eec682e2308
Zack Jameson <zacharymjameson@gmail.com>
2021-05-21 15:57:00 -0400

    are you here

---
+++ are-you-here.js
@@ -0,0 +1,16 @@
+//3. Are you here?
+
+//What is the Big O of the following algorithm? Explain your answer
+
+function areYouHere(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+}
+
+// 0(n) - completion time increases as the input data grow
